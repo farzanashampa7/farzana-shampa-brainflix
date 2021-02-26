c90ef1537dadf1a3dba29182f65890b1aeef3a48
Farzana Shampa <farzanashampa7@gmail.com>
2021-02-25 22:12:43 -0500

    Mobile header done

---
+++ Sprint-1/src/components/Comments.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+    render() {
+        return (
+            <h2>comments</h2>
+        )
+    }
+}
+
+export default Comments;
